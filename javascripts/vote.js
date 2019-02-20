70a8822b0ed2d5f31d6fb5d823fa6e30352fa4a1
László Horváth <bedafoltyn@gmail.com>
2019-02-20 16:36:43 +0100

    vote is using the new api

--- javascripts/vote.js
+++ javascripts/vote.js
@@ -3,263 +3,91 @@ const voteListResults = document.querySelector('.vote-list.results')
 const voteRadios = Array.from(document.querySelectorAll('.vote-list input'))
 const resultElements = Array.from(document.querySelectorAll('.results li div'))
 
-const resultsMock = [
-  {
-    "id": 1,
-    "vote": 3
-  },
-  {
-    "id": 2,
-    "vote": 4
-  },
-  {
-    "id": 3,
-    "vote": 3
-  },
-  {
-    "id": 4,
-    "vote": 10
-  },
-  {
-    "id": 5,
-    "vote": 3
-  },
-  {
-    "id": 6,
-    "vote": 10
-  },
-  {
-    "id": 7,
-    "vote": 8
-  },
-  {
-    "id": 8,
-    "vote": 10
-  },
-  {
-    "id": 9,
-    "vote": 7
-  },
-  {
-    "id": 10,
-    "vote": 1
-  },
-  {
-    "id": 11,
-    "vote": 6
-  },
-  {
-    "id": 12,
-    "vote": 1
-  },
-  {
-    "id": 13,
-    "vote": 2
-  },
-  {
-    "id": 14,
-    "vote": 3
-  },
-  {
-    "id": 15,
-    "vote": 2
-  },
-  {
-    "id": 16,
-    "vote": 4
-  },
-  {
-    "id": 17,
-    "vote": 10
-  },
-  {
-    "id": 18,
-    "vote": 6
-  },
-  {
-    "id": 19,
-    "vote": 4
-  },
-  {
-    "id": 20,
-    "vote": 3
-  },
-  {
-    "id": 21,
-    "vote": 8
-  },
-  {
-    "id": 22,
-    "vote": 3
-  },
-  {
-    "id": 23,
-    "vote": 5
-  },
-  {
-    "id": 24,
-    "vote": 4
-  },
-  {
-    "id": 25,
-    "vote": 1
-  },
-  {
-    "id": 26,
-    "vote": 7
-  },
-  {
-    "id": 27,
-    "vote": 10
-  },
-  {
-    "id": 28,
-    "vote": 6
-  },
-  {
-    "id": 29,
-    "vote": 6
-  },
-  {
-    "id": 30,
-    "vote": 9
-  },
-  {
-    "id": 31,
-    "vote": 4
-  },
-  {
-    "id": 32,
-    "vote": 7
-  },
-  {
-    "id": 33,
-    "vote": 10
-  },
-  {
-    "id": 34,
-    "vote": 10
-  },
-  {
-    "id": 35,
-    "vote": 9
-  },
-  {
-    "id": 36,
-    "vote": 10
-  },
-  {
-    "id": 37,
-    "vote": 9
-  },
-  {
-    "id": 38,
-    "vote": 10
-  },
-  {
-    "id": 39,
-    "vote": 8
-  },
-  {
-    "id": 40,
-    "vote": 5
-  },
-  {
-    "id": 41,
-    "vote": 10
-  },
-  {
-    "id": 42,
-    "vote": 5
-  },
-  {
-    "id": 43,
-    "vote": 7
-  },
-  {
-    "id": 44,
-    "vote": 2
-  },
-  {
-    "id": 45,
-    "vote": 3
-  },
-  {
-    "id": 46,
-    "vote": 3
-  },
-  {
-    "id": 47,
-    "vote": 1
-  },
-  {
-    "id": 48,
-    "vote": 10
-  },
-  {
-    "id": 49,
-    "vote": 10
-  },
-  {
-    "id": 50,
-    "vote": 7
-  }
-]
 let userVote = undefined
+let poll = undefined
 
-function countVotes (results) {
-  const resultsCount = {
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-    6: 0,
-    7: 0,
-    8: 0,
-    9: 0,
-    10: 0
-  }
-  
-  return results.reduce(function (accumulator, result) {
-    accumulator[result.vote] += 1
+function setUserCookie (userId) {
+  document.cookie = `poll-user=${userId}`
+}
 
-    return accumulator
-  }, resultsCount)
+function didUserVoted () {
+  return document.cookie.includes('poll-user')
+}
+
+function showError (message) {
+  console.log(message)
+}
+
+function getPoll () {
+  return fetch('http://localhost:3000/poll/5c6ad5f0b2962c15e29e012e')
+    .then(response => response.json())
+    .then(function (data) {
+      poll = data.poll
+
+      return poll
+    })
+    .then(setVoteValues)
+    .catch(error => error)
+}
+
+function setVoteValues (poll) {
+  voteRadios.map(function (vote, index) {
+    vote.value = poll.answers[index]._id
+    resultElements[index].dataset.value = poll.answers[index]._id
+    resultElements[index].dataset.count = 0
+  })
+}
+
+function countVotes () {
+  poll.votes.forEach(function (vote) {
+    const result = resultElements.find(result => result.dataset.value === vote.answer)
+    if (result) {
+      result.dataset.count++
+    }
+  })
 }
 
 function vote () {
-  const selected = voteRadios
-    .find(radio => radio.checked)
-    .value
+  const selected = voteRadios.find(radio => radio.checked).value
 
-  userVote = {id: 51, vote: parseInt(selected)}
-  resultsMock.push({id: 51, vote: parseInt(selected)})
+  if (selected) {
+    const body = JSON.stringify({answer: selected})
 
-  showResults(resultsMock, resultsMock.length)
+    return fetch('http://localhost:3000/poll/5c6ad5f0b2962c15e29e012e', {
+      body: body, 
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(showResults)
+    .catch(error => error)
+  } else {
+    showError('No answer found.')
+  }
 }
 
 function countColorLevel (number, total) {
   const percentage = number / total
-  return Math.ceil((percentage * 100) / 5) * 5
+  return Math.floor((percentage * 100) / 5) * 5
 }
 
-function showResults (results, total) {
-  const votes = countVotes(results)
+function showResults ({vote}) {
+  poll.votes = [...poll.votes, vote]
   
-  resultElements.map(function (element, index) {
-    const colorLevel = countColorLevel(votes[index + 1], total)
-    console.log(colorLevel)
+  countVotes()
+  
+  resultElements.forEach(function (element) {
+    const colorLevel = countColorLevel(element.dataset.count, poll.votes.length)
     element.classList.add(`vote-${colorLevel}`)
-
-    return element
   })
 
   voteListRadios.classList.remove('active')
   voteListResults.classList.add('active')
 
-  showUsersVote()
-  showMostVoted(votes)
+  showUsersVote(vote)
+  showMostVoted()
 }
 
 function createElement (tag, text, classNames) {
@@ -272,24 +100,20 @@ function createElement (tag, text, classNames) {
   return element
 }
 
-function showUsersVote () {
-  const element = resultElements.find(function (element, index) {
-    return index + 1 === userVote.vote
-  })
+function showUsersVote (userVote) {
+  const element = resultElements.find(result => result.dataset.value === userVote.answer)
 
   const textElement = createElement('span', 'A te véleményed', ['result-text', 'user-result'])
   element.parentNode.appendChild(textElement)
 }
 
-function showMostVoted (results) {
-  const mostVoted = Object.keys(results).reduce((a, b) => results[a] > results[b] ? a : b)
+function showMostVoted () {
+  const mostVoted = resultElements.reduce((a, b) => a.dataset.count > b.dataset.count ? a : b)
 
-  const element = resultElements.find(function (element, index) {
-    return index + 1 === parseInt(mostVoted)
-  })
-  
-  const textElement = createElement('span', 'Olvasóink átlaga', ['result-text', 'average-result'])
-  element.parentNode.appendChild(textElement)
+  const textElement = createElement('span', 'Legtöbbet választott', ['result-text', 'average-result'])
+  mostVoted.parentNode.appendChild(textElement)
 }
 
 voteRadios.forEach(radio => radio.addEventListener('change', vote))
+
+getPoll()
